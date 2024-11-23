3ddf44c7700b885ee45e9011df5201a9f57c5670
unknown <mhriad786@gmail.com>
2024-11-23 12:06:17 +0600

    setup upgrade with react router setup

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,56 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import Home from "../Pages/Home/Home";
+import AddNewService from "../Pages/AddNewService/AddNewService";
+import CartDetails from "../Pages/CartDetails/CartDetails";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import CheckOut from "../Pages/CheckOut/CheckOut";
+import ManageAllOrders from "../Pages/ManageAllOrders/ManageAllOrders";
+import ServiceDetails from "../Pages/ServiceDetails/ServiceDetails";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+        path: "/",
+        element: <Root />,
+        errorElement:<ErrorPage/>,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/add-new-service",
+                element: <AddNewService />,
+            },
+            {
+                path: "/manage-all-orders",
+                element: <ManageAllOrders />,
+            },
+            {
+                path: "/service-details",
+                element: <ServiceDetails />,
+            },
+            {
+                path: "/cart-details",
+                element: <CartDetails />,
+            },
+            {
+                path: "/checkout",
+                element: <CheckOut />,
+            },
+        ]
     },
-  ]);
+]);
 
-  export default router;
+export default router;
