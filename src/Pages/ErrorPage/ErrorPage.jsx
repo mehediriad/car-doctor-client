a52ea9b604aa47701e8a31d37cc10949f3460802
unknown <mhriad786@gmail.com>
2024-11-25 03:30:22 +0600

    not found page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,19 @@
-
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import notFound from "../../assets/notfound.png"
 
 const ErrorPage = () => {
     return (
-        <div>
-            ErrorPage
-        </div>
+        <>
+            <div className="max-w-7xl mx-auto">
+                <Header />
+                <div className="flex justify-center items-center h-screen">
+                    <img src={notFound} alt="Not Found" />
+                </div>
+
+            </div>
+            <Footer />
+        </>
     );
 };
 
